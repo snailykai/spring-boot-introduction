7a0e071d86068b61ff51ec3d482333ab3a3d95c0
kai <snailsmilesnaily@gmail.com>
2023-05-17 16:20:15 +0900

    add import Entity

--- src/main/java/com/example/entity/Employee.java
+++ src/main/java/com/example/entity/Employee.java
@@ -1,15 +1,14 @@
 package com.example.entity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-@EntityScan
+@Entity
 @Table(name = "EMPLOYEES")
 public class Employee {
     @Id
